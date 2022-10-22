51182cc0ca60550e4447eb0d9017c096d51064e9
jeongjulee <ljj3342@naver.com>
2022-10-22 20:39:41 +0900

    Feat: color change

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
@@ -29,6 +30,10 @@ function cancelPainting(event){
 function onLineWidthChange(event){
     ctx.lineWidth = event.target.value;
 }
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting)
@@ -36,3 +41,4 @@ canvas.addEventListener("mouseup", onMouseUp)
 canvas.addEventListener("mouseleave", cancelPainting)
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange)
