b39daec3a8ded3949a69f9444fe0da98d0082db2
jeongjulee <ljj3342@naver.com>
2022-10-22 20:23:16 +0900

    Feat: draw lines

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff",
-];
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return ;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 
-function onClick(event){
-    ctx.beginPath();
-    ctx.moveTo(0, 0)
-    const color = colors[Math.floor(Math.random()*colors.length)]
-    ctx.strokeStyle = color
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
 }
-canvas.addEventListener("mousemove", onClick);
+function startPainting(event){
+    isPainting = true;
+}
+function onMouseUp(event){
+    isPainting = false;
+}
+function cancelPainting(event){
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting)
+canvas.addEventListener("mouseup", onMouseUp)
+canvas.addEventListener("mouseleave", cancelPainting)
 
